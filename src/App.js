9b20bb488a481edbca34b9c34ec95cd93cc03d6b
hidemi2008 <gabrielhidemi2@gmail.com>
2025-06-14 22:06:59 -0400

    criacao da aplicacao com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactPlayer from 'react-player';
 
-function App() {
+import "./css/App.css"
+
+const VideoPlayer = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+              <div >
+                  </div>
+                      <div>
+                        <ReactPlayer  className='react-player' url='https://www.youtube.com/watch?v=oUFJJNQGwhk'/>
+                  </div>
     </div>
   );
-}
+};
 
-export default App;
+export default VideoPlayer;
