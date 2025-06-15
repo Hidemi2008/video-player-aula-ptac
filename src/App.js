5f2fa3177575e0455e5606b4c73e7113c72b8fe7
hidemi2008 <gabrielhidemi2@gmail.com>
2025-06-14 22:30:51 -0400

    completei o display dos videos e criei botoes de alternativas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 
-import "./css/App.css"
+import './css/App.css';
 
 const VideoPlayer = () => {
+  const videos = [
+    { title: 'Vídeo 1', url: 'https://youtu.be/Rr8ljRgcJNM?si=EP-CdI5Ba0hK6sx-' },
+    { title: 'Vídeo 2', url: 'https://youtu.be/W3q8Od5qJio?si=w8vRJdLvzh9SL3kA' },
+    { title: 'Vídeo 3', url: 'https://youtu.be/NeQM1c-XCDc?si=CJnESkAJai6skZQB' }
+  ];
+
+  const [currentVideo, setCurrentVideo] = useState(videos[0].url);
+
   return (
-   <div>
-              <div >
-                  </div>
-                      <div>
-                        <ReactPlayer  className='react-player' url='https://www.youtube.com/watch?v=oUFJJNQGwhk'/>
-                  </div>
+    <div className="div-video">
+      <div className="video">
+        <ReactPlayer
+          url={currentVideo}
+          controls
+          width="100%"
+          height="100%"
+          className="react-player"
+        />
+      </div>
+
+      <div className="video-list">
+        {videos.map((video, index) => (
+          <button
+            key={index}
+            className="video-button"
+            onClick={() => setCurrentVideo(video.url)}
+          >
+            {video.title}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
